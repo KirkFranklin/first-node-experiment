c1e6ef95eeef9d04ea20fa612c534738e67fbd4f
Mukabwa <kfranklyn20@gmail.com>
2024-06-18 13:46:20 -0400

    first-node-experiment

---
+++ test.js
@@ -0,0 +1,31 @@
+let express = require("express")
+let ourApp = express()
+
+ourApp.use(express.urlencoded({extended: false}))
+ourApp.get('/', function(req, res) {
+    res.send(`
+    <form action="/answer" method="POST">
+        <p>What color is the sky on a clear and sunny day?</p>
+        <input name="skyColor" autocomplete="off">
+        <button>Submit Answer</button>
+    </form>
+    `)
+})
+ourApp.post('/answer', function(req, res){
+    if (req.body.skyColor.toUpperCase() == "BLUE") {
+        res.send(`
+        <p>Congrats, that is the correct answer!</p>
+        <a href="/">Back to homepage</a>
+        `)
+    } else {
+        res.send(`
+        <p>Sorry, that is incorrect.</p>
+        <a href="/">Back to homepage</a>
+        `)
+    }
+})
+
+ourApp.get('/answer', function(req, res){
+    res.send("Are you lost? There is nothing to see here.")
+})
+ourApp.listen(3000)
